3fe2446395f56e43fc271819f64d2932025aa778
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-08-24 13:58:16 -0400

    Starting to get angular wired up. Next I will be removing scope

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-var app = angular.module('todo',[]);
+var app = angular.module('ToDo',[]);
+
+
+app.controller('todoController', ['$scope',function($scope){
+  $scope.todos = [
+  {'title': 'Build a todo app', 'done': false}
+];
+
+$scope.addTodo  = function(){}
+$scope.clearCompleted = function(){}
+
+
+
+}]);
