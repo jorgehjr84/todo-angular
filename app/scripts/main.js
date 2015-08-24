638176a3caffe6f839516074c1945350465c2b6a
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-08-24 14:14:47 -0400

    You can now add new todos and the list clears after a todo is submitted

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,13 @@ app.controller('todoController', ['$scope',function($scope){
   {'title': 'Build a todo app', 'done': false}
 ];
 
-$scope.addTodo  = function(){}
+$scope.addTodo  = function(){
+
+  $scope.todos.push({'title':$scope.newTodo, 'done':false})
+  $scope.newTodo = '';
+};
+
+
 $scope.clearCompleted = function(){}
 
 
